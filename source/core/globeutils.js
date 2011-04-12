e87ca2c4ab3ba5b9aec86bd20a9b20b8130674bb
Benjamin Loesch <benjamin.loesch@fhnw.ch>
2011-04-12 16:18:52 +0200

    utility and conversion functions added.

---
+++ source/core/globeutils.js
@@ -0,0 +1,86 @@
+/*******************************************************************************
+#      ____               __          __  _      _____ _       _               #
+#     / __ \              \ \        / / | |    / ____| |     | |              #
+#    | |  | |_ __   ___ _ __ \  /\  / /__| |__ | |  __| | ___ | |__   ___      #
+#    | |  | | '_ \ / _ \ '_ \ \/  \/ / _ \ '_ \| | |_ | |/ _ \| '_ \ / _ \     #
+#    | |__| | |_) |  __/ | | \  /\  /  __/ |_) | |__| | | (_) | |_) |  __/     #
+#     \____/| .__/ \___|_| |_|\/  \/ \___|_.__/ \_____|_|\___/|_.__/ \___|     #
+#           | |                                                                #
+#           |_|                 _____ _____  _  __                             #
+#                              / ____|  __ \| |/ /                             #
+#                             | (___ | |  | | ' /                              #
+#                              \___ \| |  | |  <                               #
+#                              ____) | |__| | . \                              #
+#                             |_____/|_____/|_|\_\                             #
+#                                                                              #
+#                              (c) 2010-2011 by                                #
+#           University of Applied Sciences Northwestern Switzerland            #
+#                           martin.christen@fhnw.ch                            #
+********************************************************************************
+
+This file is part of the OpenWebGlobe SDK
+
+GPL LICENSE
+
+i3D OpenWebGlobe SDK is free software: you can redistribute it and/or modify  it
+under the  terms of  the GNU  General Public  License as  published by  the Free
+Software Foundation, either version  2 of the License,  or (at your option)  any
+later version.
+
+i3D OpenWebGlobe  SDK is  distributed in  the hope  that it  will be useful, but
+WITHOUT ANY WARRANTY;  without even the  implied warranty of  MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See  the GNU General Public License for  more
+details.
+
+You should have received a copy of the GNU General Public License along with i3D
+OpenWebGlobe SDK.  If not, see <http://www.gnu.org/licenses/>.
+
+As a special  exception to the  GPL, any HTML  file which merely  makes function
+calls to  this code,  and for  that purpose  includes it  by reference, shall be
+deemed a separate work for copyright law purposes. If you modify this code,  you
+may extend this exception to your version of the code, but you are not obligated
+to do so. If you do not wish to do so, delete this exception statement from your
+version.
+
+Commercial License
+
+OEMs (Original  Equipment Manufacturers),  ISVs (Independent  Software Vendors),
+VARs (Value Added Resellers) and other distributors that combine and  distribute
+commercially licensed  software with  i3D OpenWebGlobe  SDK and  do not  wish to
+distribute the source code for the commercially licensed software under  version
+2 of the  GNU General Public  License (the "GPL")  must enter into  a commercial
+license agreement with the Institute of Geomatics Engineering at the  University
+of Applied Sciences Northwestern Switzerland (FHNW).
+*******************************************************************************/
+
+
+//------------------------------------------------------------------------------
+/**
+ * @constructor
+ * @class
+ * @description This is a class including some utility functions.
+ * @author Martin Christen, martin.christen@fhnw.ch
+ * @author Benjamin Loesch, benjamin.loesch@fhnw.ch
+ * @version 0.1
+ */
+var GlobeUtils = {       //Static class initialisation...JSON Style. 
+   
+      "MakeHierarchicalFilename" : function(filename)
+      {
+        
+         var newstring = "";
+         for(var i=0;i < filename.length; i++)
+         { 
+            if(i%2==0 && i>0 && i<filename.length-4)
+            {
+               newstring = newstring.concat("/");    
+            }  
+            newstring = newstring.concat(filename[i]);
+         }
+         
+         return newstring;  
+      } 
+     
+}
+
+
