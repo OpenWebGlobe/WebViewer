5c2a77a0aa63cb4d82484374d415b4f6c9b3f03a
Martin Christen <martin.christen@fhnw.ch>
2011-05-01 09:44:46 +0200

    fixed issue when globe is not available yet

--- source/core/globerenderer.js
+++ source/core/globerenderer.js
@@ -497,7 +497,7 @@ GlobeRenderer.prototype.AltitudeAboveGround = function()
       return dist;
    }
 
-   
+   return 0;
    
 }
  //-----------------------------------------------------------------------------
