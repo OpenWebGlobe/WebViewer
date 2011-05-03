e868c6ea4672653033e0a91984fd1e23cf1cf7ac
Tom Payne <tom.payne@camptocamp.com>
2011-05-03 17:09:41 +0200

    Rely on compiler to ensure type compliance

--- source/core/mat4.js
+++ source/core/mat4.js
@@ -429,38 +429,35 @@ mat4.prototype.LookAt = function(eyex, eyey, eyez, centerx, centery, centerz, up
  */ 
 mat4.prototype.Multiply = function(a,b)
 {
-   if (a instanceof mat4 && b instanceof mat4)
-   {
-      var a00 = a._values[0],  a01 = a._values[1],  a02 = a._values[2],  a03 = a._values[3];
-      var a10 = a._values[4],  a11 = a._values[5],  a12 = a._values[6],  a13 = a._values[7];
-      var a20 = a._values[8],  a21 = a._values[9],  a22 = a._values[10], a23 = a._values[11];
-      var a30 = a._values[12], a31 = a._values[13], a32 = a._values[14], a33 = a._values[15];
+   var a00 = a._values[0],  a01 = a._values[1],  a02 = a._values[2],  a03 = a._values[3];
+   var a10 = a._values[4],  a11 = a._values[5],  a12 = a._values[6],  a13 = a._values[7];
+   var a20 = a._values[8],  a21 = a._values[9],  a22 = a._values[10], a23 = a._values[11];
+   var a30 = a._values[12], a31 = a._values[13], a32 = a._values[14], a33 = a._values[15];
+
+   var b00 = b._values[0],  b01 = b._values[1],  b02 = b._values[2],  b03 = b._values[3];
+   var b10 = b._values[4],  b11 = b._values[5],  b12 = b._values[6],  b13 = b._values[7];
+   var b20 = b._values[8],  b21 = b._values[9],  b22 = b._values[10], b23 = b._values[11];
+   var b30 = b._values[12], b31 = b._values[13], b32 = b._values[14], b33 = b._values[15];
    
-      var b00 = b._values[0],  b01 = b._values[1],  b02 = b._values[2],  b03 = b._values[3];
-      var b10 = b._values[4],  b11 = b._values[5],  b12 = b._values[6],  b13 = b._values[7];
-      var b20 = b._values[8],  b21 = b._values[9],  b22 = b._values[10], b23 = b._values[11];
-      var b30 = b._values[12], b31 = b._values[13], b32 = b._values[14], b33 = b._values[15];
-      
-      this._values[0] = b00*a00 + b01*a10 + b02*a20 + b03*a30;
-      this._values[1] = b00*a01 + b01*a11 + b02*a21 + b03*a31;
-      this._values[2] = b00*a02 + b01*a12 + b02*a22 + b03*a32;
-      this._values[3] = b00*a03 + b01*a13 + b02*a23 + b03*a33;
-      
-      this._values[4] = b10*a00 + b11*a10 + b12*a20 + b13*a30;
-      this._values[5] = b10*a01 + b11*a11 + b12*a21 + b13*a31;
-      this._values[6] = b10*a02 + b11*a12 + b12*a22 + b13*a32;
-      this._values[7] = b10*a03 + b11*a13 + b12*a23 + b13*a33;
-      
-      this._values[8] = b20*a00 + b21*a10 + b22*a20 + b23*a30;
-      this._values[9] = b20*a01 + b21*a11 + b22*a21 + b23*a31;
-      this._values[10] = b20*a02 + b21*a12 + b22*a22 + b23*a32;
-      this._values[11] = b20*a03 + b21*a13 + b22*a23 + b23*a33;
-      
-      this._values[12] = b30*a00 + b31*a10 + b32*a20 + b33*a30;
-      this._values[13] = b30*a01 + b31*a11 + b32*a21 + b33*a31;
-      this._values[14] = b30*a02 + b31*a12 + b32*a22 + b33*a32;
-      this._values[15] = b30*a03 + b31*a13 + b32*a23 + b33*a33;
-   }
+   this._values[0] = b00*a00 + b01*a10 + b02*a20 + b03*a30;
+   this._values[1] = b00*a01 + b01*a11 + b02*a21 + b03*a31;
+   this._values[2] = b00*a02 + b01*a12 + b02*a22 + b03*a32;
+   this._values[3] = b00*a03 + b01*a13 + b02*a23 + b03*a33;
+   
+   this._values[4] = b10*a00 + b11*a10 + b12*a20 + b13*a30;
+   this._values[5] = b10*a01 + b11*a11 + b12*a21 + b13*a31;
+   this._values[6] = b10*a02 + b11*a12 + b12*a22 + b13*a32;
+   this._values[7] = b10*a03 + b11*a13 + b12*a23 + b13*a33;
+   
+   this._values[8] = b20*a00 + b21*a10 + b22*a20 + b23*a30;
+   this._values[9] = b20*a01 + b21*a11 + b22*a21 + b23*a31;
+   this._values[10] = b20*a02 + b21*a12 + b22*a22 + b23*a32;
+   this._values[11] = b20*a03 + b21*a13 + b22*a23 + b23*a33;
+   
+   this._values[12] = b30*a00 + b31*a10 + b32*a20 + b33*a30;
+   this._values[13] = b30*a01 + b31*a11 + b32*a21 + b33*a31;
+   this._values[14] = b30*a02 + b31*a12 + b32*a22 + b33*a32;
+   this._values[15] = b30*a03 + b31*a13 + b32*a23 + b33*a33;
 }
 
 //------------------------------------------------------------------------------
@@ -494,28 +491,25 @@ mat4.prototype.Transpose = function()
  */ 
 mat4.prototype.MultiplyVec3 = function(vec)
 {
-   if(vec instanceof vec3)
+   var resVec;
+   if(this._values instanceof Float32Array)
    {
-      var resVec;
-      if(this._values instanceof Float32Array)
-      {
-         resVec = new vec3("float"); 
-      }
-      else
-      {
-       resVec = new vec3("double");
-      }  
-      resVec._values[0]=this._values[0]*vec._values[0]+this._values[4]*vec._values[1]+this._values[8]*vec._values[2]+this._values[12];
-      resVec._values[1]=this._values[1]*vec._values[0]+this._values[5]*vec._values[1]+this._values[9]*vec._values[2]+this._values[13];
-      resVec._values[2]=this._values[2]*vec._values[0]+this._values[6]*vec._values[1]+this._values[10]*vec._values[2]+this._values[14];
-      var w=this._values[3]*vec._values[0]+this._values[7]*vec._values[1]+this._values[11]*vec._values[2]+this._values[15]; 
-      
-      resVec._values[0]=resVec._values[0]/w;
-      resVec._values[1]=resVec._values[1]/w;
-      resVec._values[2]=resVec._values[2]/w;
-             
-      return resVec;
+      resVec = new vec3("float");
    }
+   else
+   {
+    resVec = new vec3("double");
+   }
+   resVec._values[0]=this._values[0]*vec._values[0]+this._values[4]*vec._values[1]+this._values[8]*vec._values[2]+this._values[12];
+   resVec._values[1]=this._values[1]*vec._values[0]+this._values[5]*vec._values[1]+this._values[9]*vec._values[2]+this._values[13];
+   resVec._values[2]=this._values[2]*vec._values[0]+this._values[6]*vec._values[1]+this._values[10]*vec._values[2]+this._values[14];
+   var w=this._values[3]*vec._values[0]+this._values[7]*vec._values[1]+this._values[11]*vec._values[2]+this._values[15];
+
+   resVec._values[0]=resVec._values[0]/w;
+   resVec._values[1]=resVec._values[1]/w;
+   resVec._values[2]=resVec._values[2]/w;
+
+   return resVec;
 }
 
 //------------------------------------------------------------------------------
