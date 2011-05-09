13fc0ecd54a47ffbb7ad2a7bf801aed7af6379c3
Benjamin Loesch <benjamin.loesch@fhnw.ch>
2011-05-09 16:42:22 +0200

    toolbox added.

--- source/demos/03_EnhancedWorldDemo/tools/toolbox.js
+++ source/demos/03_EnhancedWorldDemo/tools/toolbox.js
@@ -1,58 +1,77 @@
+/*******************************************************************************
+#      ____               __          __  _      _____ _       _               #
+#     / __ \              \ \        / / | |    / ____| |     | |              #
+#    | |  | |_ __   ___ _ __ \  /\  / /__| |__ | |  __| | ___ | |__   ___      #
+#    | |  | | '_ \ / _ \ '_ \ \/  \/ / _ \ '_ \| | |_ | |/ _ \| '_ \ / _ \     #
+#    | |__| | |_) |  __/ | | \  /\  /  __/ |_) | |__| | | (_) | |_) |  __/     #
+#     \____/| .__/ \___|_| |_|\/  \/ \___|_.__/ \_____|_|\___/|_.__/ \___|     #
+#           | |                                                                #
+#           |_|                 _____ _____  _  __                             #
+#                              / ____|  __ \| |/ /                             #
+#                             | (___ | |  | | ' /                              #
+#                              \___ \| |  | |  <                               #
+#                              ____) | |__| | . \                              #
+#                             |_____/|_____/|_|\_\                             #
+#                                                                              #
+#                              (c) 2010-2011 by                                #
+#           University of Applied Sciences Northwestern Switzerland            #
+#                     Institute of Geomatics Engineering                       #
+#                           martin.christen@fhnw.ch                            #
+********************************************************************************
+*     Licensed under MIT License. Read the file LICENSE for more information   *
+*******************************************************************************/      
 
 
-/**
- * Toolbox handling class.
+/** 
+ * @class ToolBox 
+ * @description  Creates a Toolbox and handles the tools object activeness.
+ * @constructor
+ * 
+ * {@link http://www.openwebglobe.org} 
+ * 
+ * @author Benjamin Loesch benjamin.loesch@fhnw.ch 
+ * @version 0.1  
  */
-
-function ToolBox(tbname)
+function ToolBox(tbname, canvasId)
 {
    //create toolbox panel 
    this.name = tbname;
+   this.canvasId = canvasId;
    this.div = document.createElement('div');
-   this.div.id = 'toolbox';  //todo: think about this...
-   this.positionX = 400;
-   this.positionY = 400;
+   this.div.id = tbname;
+    document.body.appendChild(this.div);
+   
+   
+   this.positionX = parseInt(getStyle(this.div,'left'));
+   this.positionY = parseInt(getStyle(this.div,'top'));
+  
    
    this.dragOffsetX = 0;
-   this.dragOffsetY = 0;
-   
-   
-   
-   
-   //todo: dragging implementieren
- 
+   this.dragOffsetY = 0;  
  
    var tb = this;
    this.div.onmousedown = function(e){tb.StartDrag(e)};
    this.div.onmouseup = function(e){tb.StopDrag(e)};
-   this.cbfDrag = function(e){tb.MouseMove(e)};
-
-   document.body.appendChild(this.div);
+   this.cbfDrag = function(e){tb.MouseMove(e)}; 
 
    this.tools = [];
    
-   
+    
    //add tools
-   pickCoordinates = new PickCoordinatesTool(this,40,40,10,40);
+   
+   pickCoordinates = new CoordinatesTool(this,40,40,10,40);
    pickCoordinates.SetActiveCallback(this.cbfToolSetActive);
    this.tools.push(pickCoordinates);
-  /* 
-   pickCoordinates2 = new PickCoordinatesTool(this,20,20,30,5);
-   pickCoordinates2.SetActiveCallback(this.cbfToolSetActive);
-   this.tools.push(pickCoordinates2);
    
-   
-   pickCoordinates3 = new PickCoordinatesTool(this,20,20,5,30);
-   pickCoordinates3.SetActiveCallback(this.cbfToolSetActive);
-   this.tools.push(pickCoordinates3);
-   
-   pickCoordinates4 = new PickCoordinatesTool(this,20,20,30,30);
-   pickCoordinates4.SetActiveCallback(this.cbfToolSetActive);
-   this.tools.push(pickCoordinates4);
-  */
+   //insert more tools here... 
+  
 }
 
-
+//------------------------------------------------------------------------------
+/**
+ * @description This function will be called from a tool-object if one of the tools is set to active.
+ * 
+ */
 ToolBox.prototype.cbfToolSetActive = function()
 {
    //set all others inactive
@@ -61,12 +80,16 @@ ToolBox.prototype.cbfToolSetActive = function()
       if(this.toolbox.tools[i] != this)
       {
          this.toolbox.tools[i].SetInactive();
-      }
-      
+      }     
    }  
 }
 
-
+//------------------------------------------------------------------------------
+/**
+ * @description Start dragging the toolbox. 
+ * @param {object} event data with mouse-position.
+ * 
+ */
 ToolBox.prototype.StartDrag = function(eventData)
 {
    this.startDragX = eventData.clientX;
@@ -77,6 +100,11 @@ ToolBox.prototype.StartDrag = function(eventData)
    //store offset
 }
 
+//------------------------------------------------------------------------------
+/**
+ * @description Stop dragging the toolbox. 
+ * 
+ */
 ToolBox.prototype.StopDrag = function()
 {
     var tb = this;
@@ -86,15 +114,40 @@ ToolBox.prototype.StopDrag = function()
     console.log("stop dragging toolbox");
 }
 
+//------------------------------------------------------------------------------
+/**
+ * @description If drag-mode this function moves the div.
+ * @param {object} event data with mouse-position.
+ */
 ToolBox.prototype.MouseMove = function(eventData)
 {
-   
    this.div.style.left = this.positionX-(this.startDragX-eventData.clientX)+'px';  //calc offset;
    this.div.style.top = this.positionY-(this.startDragY-eventData.clientY)+'px';
    this.newPositionX = this.positionX-(this.startDragX-eventData.clientX);
    this.newPositionY = this.positionY-(this.startDragY-eventData.clientY);
 }
 
+
+
+//------------------------------------------------------------------------------
+/**
+ * @description Function to get specific css style attribute.
+ * @param {html-object} the html object e.g. a 'div'-element
+ * @param {string} css attribute.
+ */
+function getStyle(oElm, strCssRule){
+	var strValue = "";
+	if(document.defaultView && document.defaultView.getComputedStyle){
+		strValue = document.defaultView.getComputedStyle(oElm, "").getPropertyValue(strCssRule);
+	}
+	else if(oElm.currentStyle){
+		strCssRule = strCssRule.replace(/\-(\w)/g, function (strMatch, p1){
+			return p1.toUpperCase();
+		});
+		strValue = oElm.currentStyle[strCssRule];
+	}
+	return strValue;
+}
 
 
 
