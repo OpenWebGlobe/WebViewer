f18674a8222e7c07d9ead7bfeb1009fcfc93b9d9
Martin Christen <martin.christen@fhnw.ch>
2011-10-20 19:14:57 +0200

    slider is now moving as long as mouse button is pressed

--- source/core/nodes/logos.js
+++ source/core/nodes/logos.js
@@ -170,6 +170,11 @@ function LogosNode()
             {
                this.texSlider.Blit(xpos-16, ypos-16, 0, 0, 1, 1, true);
             }
+            
+            
+            ypos = this.engine.height-1-72-128-64-64;
+            this.compassr.Blit(xpos-32, ypos-32,0,this.yaw,0.5,0.5,true);    
+            this.compassbg.Blit(xpos-32, ypos-32,0,0,0.5,0.5,true);
          }
          
          
@@ -319,6 +324,23 @@ function LogosNode()
          var slider_x1 = slider_x0 + 16;
          var slider_y0 = slider_y1 - 16;
       
+        
+         // move slider
+         if (this.btn && this.navigationState == 6)
+         {
+            this.sliderYPos -= dy;
+            if (this.sliderYPos<-40)
+            {
+               this.sliderYPos = -40;
+            }
+            else if (this.sliderYPos>40)
+            {
+               this.sliderYPos = 40;
+            }
+            
+            return;
+         }
+      
          this.navigationState = 0;
          
          if (this.mouseX > plus_x0 &&
@@ -360,17 +382,6 @@ function LogosNode()
          {
             if (this.btn)
             {   
-               // move slider
-
-               this.sliderYPos -= dy;
-               if (this.sliderYPos<-40)
-               {
-                    this.sliderYPos = -40;
-               }
-               else if (this.sliderYPos>40)
-               {
-                    this.sliderYPos = 40;
-               }
                this.navigationState = 6; // slider pressed
             }
             else
